7e5e9a97da221f4ff7c1f266de2c8691dad0e678
Valerio <valerio.delmeglio@gmail.com>
2014-01-29 23:32:54 +0100

    Fixed the template rendering

--- blogger/urls.py
+++ blogger/urls.py
@@ -8,8 +8,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
    (r'^admin/', include(admin.site.urls)),
    (r'^$', 'blogger.blog.views.index'),
+url(r'^postForm.html', 'blogger.blog.views.view_form', name='view_blog_form'),    
 url(r'^blog/view/(?P<slug>[^\.]+).html', 'blogger.blog.views.view_post', name='view_blog_post'),
-url(r'^blog/category/(?P<slug>[^\.]+).html', 'blogger.blog.views.view_category', name='view_blog_category'),
-url(r'^blog/postForm.html', 'blogger.blog.views.view_form', name='view_blog_form'),    
+url(r'^(?P<slug>[^\.]+).html', 'blogger.blog.views.view_category', name='view_blog_category'),
 
 )
