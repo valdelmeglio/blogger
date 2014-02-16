9f8945a90cbaf933bae91ab097c840dad6465abd
Valerio <valerio.delmeglio@gmail.com>
2014-02-16 16:32:13 +0100

    Implemented the single post page rendering

--- blogger/blog/views.py
+++ blogger/blog/views.py
@@ -13,12 +13,18 @@ def index(request):
         'categories': Category.objects.all(),
         'posts': Blog.objects.all()
     })
-
+'''
 def view_post(request, slug):   
     return render_to_response('view_post.html', {
         'post': get_object_or_404(Blog, slug=slug)
     })
-
+'''
+def view_post(request, id):
+    post = Blog.objects.get(id = int(id))
+    context = {'post': post}
+    #return render(request, 'view_post.html', context)
+    return render_to_response('view_post.html', {'post': post}) 
+       
 def view_category(request, slug):
     category = get_object_or_404(Category, slug=slug)
     return render_to_response('view_category.html', {
--- blogger/urls.py
+++ blogger/urls.py
@@ -10,7 +10,8 @@ urlpatterns = patterns('',
    (r'^$', 'blogger.blog.views.index'),
    (r'^login', 'django.contrib.auth.views.login'),
 url(r'^view_form', 'blogger.blog.views.view_form', name='view_form'),    
-url(r'^blog/view/(?P<slug>[^\.]+).html', 'blogger.blog.views.view_post', name='view_blog_post'),
+#url(r'^blog/view/(?P<slug>[^\.]+).html', 'blogger.blog.views.view_post', name='view_blog_post'),
+url(r'^(?P<id>[^\.]+).html', 'blogger.blog.views.view_post', name='view_blog_post'),
 url(r'^(?P<slug>[^\.]+).html', 'blogger.blog.views.view_category', name='view_blog_category'),
 
 )
