53f0ed0ac6883483b095c18fdfa4414d23bcd16b
Valerio <valerio.delmeglio@gmail.com>
2014-03-25 23:27:34 +0100

    Writing some tests

--- blogger/blog/tests.py
+++ blogger/blog/tests.py
@@ -6,11 +6,45 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from blogger.blog.models import Blog, Category
+'''
+class BlogPostFactory(object):
+    def create(self, save=False):
+        category = Category()
+        category.title = "categoria prova"
+        category.slug = "categoria prova"
+        blogpost = Blog()
+        blogpost.title = "TestPost"
+        blogpost.slug  ="TestPost"
+        blogpost.body = "This is a test"
+        blogpost.category = category
+        
+        if save==True:
+            blogpost.save()
 
+        return blogpost
+ '''       
+class CategoryFactory(object):
+    def create(self, save=False):
+        category = Category()
+        category.title = "categoria prova"      
+        category.slug = "categoria prova"
+        
+        if save==True:
+            category.save()
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+        return category
+        
+                
+            
+class BlogTest(TestCase):
+    def setUp(self):
+        pass
+    '''
+    def test_post_creation(self):
+        blogpost = BlogPostFactory().create(True)
+        self.assertTrue(blogpost.id > 0, "BlogPost created correctly")
+    '''
+    def test_category_creation(self):
+        category = CategoryFactory().create(True)
+        self.assertTrue(category.id > 0, "Category created correctly")
